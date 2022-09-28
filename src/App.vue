723cdeae4709209344d81e4d2010bb038d2928e5
tetsuharu0911 <tetsuharu.nasu@gmail.com>
2022-09-28 23:08:17 +0900

    first commit

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<script setup>
+import TheHeader from './components/TheHeader.vue';
+import TheFooter from './components/TheFooter.vue';
+</script>
+
+<template>
+  <div class="wrap">
+    <TheHeader />
+    <main class="main">メイン</main>
+    <TheFooter />
+  </div>
+</template>
+
+<style>
+@import 'assets/css/destyle.css';
+.wrap {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  min-height: 100vh;
+  width: 370px;
+  margin: 0 auto;
+  font-family: sans-serif;
+}
+.main {
+  flex: 1;
+  width: 100%;
+}
+</style>
---
+++ src/components/TheFooter.vue
@@ -0,0 +1,12 @@
+<template>
+  <fotter class="footer">
+    <small>&copy;my-vite-todo</small>
+  </fotter>
+</template>
+
+<style scoped>
+.footer {
+  margin-top: 15px;
+  height: 30px;
+}
+</style>
